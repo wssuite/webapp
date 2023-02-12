3bcac062076db71da7e5e6dbdcb529b065ddc6e4
Mark Bekhet <markonsy72@gmail.com>
2023-02-12 14:33:15 -0500

    Mock the pymongo interface used by the app module

--- test/controllers/test_nurse_controller.py
+++ test/controllers/test_nurse_controller.py
@@ -1,7 +1,9 @@
-from unittest.mock import patch
+from unittest.mock import Mock
 import app
 from unittest import TestCase
-from constants import nurse_name, nurse_contracts
+from constants import nurse_name, nurse_contracts, nurse_id
+from src.dao.abstract_dao import connect_to_fake_db
+from src.dao.nurse_dao import NurseDao
 
 
 class TestNurseController(TestCase):
@@ -11,13 +13,19 @@ class TestNurseController(TestCase):
             nurse_name: "random",
             nurse_contracts: ["fulltime"]
         }
+        app.nurse_controller.nurse_dao = Mock()
+        app.nurse_controller.nurse_dao = NurseDao(connect_to_fake_db())
+        self.dao = app.nurse_controller.nurse_dao
 
     def tearDown(self) -> None:
         pass
 
-    @patch("src.controllers.nurse_controller.nurse_dao")
-    def test_register_nurse(self, mock_dao):
+    def test_register_nurse(self):
         path = "/nurse/add"
-        self.client.post(path,
-                         json=self.nurse_dict)
-        mock_dao.insert_one.assert_called_with(self.nurse_dict)
+
+        inserted_nurse_id = self.client.post(path,
+                                             json=self.nurse_dict)
+        result_nurse = self.dao.collection.find_one(
+            {nurse_id: inserted_nurse_id.data.decode()},
+            {"_id": 0, nurse_id: 0})
+        self.assertEqual(result_nurse, self.nurse_dict)
