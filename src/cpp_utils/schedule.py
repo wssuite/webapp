9e040c2549c15cd5e4e71d59d5f90ed4a501b9d2
Mark Bekhet <markonsy72@gmail.com>
2023-02-12 20:31:32 -0500

    Update the response to the schedule endpoint to the front end needs

--- src/cpp_utils/schedule.py
+++ src/cpp_utils/schedule.py
@@ -1,22 +1,33 @@
 from src.cpp_utils.assignment import Assignment
 import re
 
-regex = r'{([a-zA-Z0-9\-]+),([a-zA-Z0-9\-]+),' \
-        r'([a-zA-Z0-9\-]+),([a-zA-Z0-9\-]+)}'
+regex_assignments = r'([a-zA-Z0-9\-]+),([a-zA-Z0-9\-]+),' \
+                    r'([a-zA-Z0-9\-]+),([a-zA-Z0-9\-]+)'
+
+regex_instance_info = r'([a-zA-Z0-9\-\.]+),([a-zA-Z0-9\-]+),' \
+                      r'([a-zA-Z0-9\-]+)'
 
 
 class Schedule:
 
     def __init__(self, file_name):
         self.assignments_list = []
+        self.start_date = ""
+        self.end_date = ""
         with open(file_name) as stream:
             reader = stream.readlines()
             for row in reader:
-                match = re.search(regex, row)
-                if match:
-                    groups = [match.group(1), match.group(2),
-                              match.group(3), match.group(4)]
+                match_regex_assignments = re.search(regex_assignments, row)
+                match_regex_instance_info = re.search(regex_instance_info, row)
+                if match_regex_assignments:
+                    groups = [match_regex_assignments.group(1),
+                              match_regex_assignments.group(2),
+                              match_regex_assignments.group(3),
+                              match_regex_assignments.group(4)]
                     self.assignments_list.append(Assignment(groups))
+                elif match_regex_instance_info:
+                    self.start_date = match_regex_instance_info.group(2)
+                    self.end_date = match_regex_instance_info.group(3)
 
     def filter_by_name(self):
         dict_filtered_name = {}
@@ -25,14 +36,20 @@ class Schedule:
                 list_assignments = [assignment.to_json()]
                 dict_filtered_name[assignment.employee_name] = list_assignments
             else:
-                list_assignments = dict_filtered_name.\
+                list_assignments = dict_filtered_name. \
                     get(assignment.employee_name)
                 list_assignments.append(assignment.to_json())
                 dict_filtered_name[assignment.employee_name] = list_assignments
-        ret = []
+        schedule = []
         for key in dict_filtered_name.keys():
             ret_element = {"employee_name": key,
                            "assignments": dict_filtered_name.get(key)
                            }
-            ret.append(ret_element)
+            schedule.append(ret_element)
+
+        ret = {
+            "startDate": self.start_date,
+            "endDate": self.end_date,
+            "schedule": schedule
+        }
         return ret
--- test/controllers/test_schedule_controller.py
+++ test/controllers/test_schedule_controller.py
@@ -7,10 +7,12 @@ from src.utils.file_system_manager import dataset_directory, base_directory
 instance = "prototype"
 version = "1"
 solution_file = "sol.txt"
-solution_content = """Assignments = 1
-{2010-06-01,Patrick,Late,Nurse}
-{2010-06-05,Patrick,Early,Nurse}
-{2010-06-03,Eve,Late,HeadNurse}"""
+solution_content = """
+instance1,2010-06-01,2010-06-28
+Assignments = 3
+2010-06-01,Patrick,Late,Nurse
+2010-06-05,Patrick,Early,Nurse
+2010-06-03,Eve,Late,HeadNurse"""
 
 
 def create_dir(fake_fs):
@@ -37,31 +39,35 @@ class TestScheduleController(TestCase):
     def test_get_schedule_filtered_by_name_if_no_errors_succeed(self, fake_fs):
         create_dir(fake_fs)
         create_sol_file(fake_fs)
-        dict_response = [
-            {"employee_name": "Patrick",
-             "assignments": [
-                 {
-                     "date": "2010-06-01",
-                     "employee_name": "Patrick",
-                     "shift": "Late",
-                     "skill": "Nurse"
+        dict_response = {
+            "startDate": "2010-06-01",
+            "endDate": "2010-06-28",
+            "schedule": [
+                {"employee_name": "Patrick",
+                 "assignments": [
+                     {
+                         "date": "2010-06-01",
+                         "employee_name": "Patrick",
+                         "shift": "Late",
+                         "skill": "Nurse"
+                     },
+                     {
+                         "date": "2010-06-05",
+                         "employee_name": "Patrick",
+                         "shift": "Early",
+                         "skill": "Nurse"
+                     }]
                  },
-                 {
-                     "date": "2010-06-05",
-                     "employee_name": "Patrick",
-                     "shift": "Early",
-                     "skill": "Nurse"
-                 }]
-             },
-            {"employee_name": "Eve",
-             "assignments": [
-                 {
-                     "date": "2010-06-03",
-                     "employee_name": "Eve",
-                     "shift": "Late",
-                     "skill": "HeadNurse"
-                 }
-             ]}]
+                {"employee_name": "Eve",
+                 "assignments": [
+                     {
+                         "date": "2010-06-03",
+                         "employee_name": "Eve",
+                         "shift": "Late",
+                         "skill": "HeadNurse"
+                     }
+                 ]}
+            ]}
         path = f"/schedule/nameFilter/{instance}?version=1"
         response = self.client.get(path)
         self.assertEqual(response.status_code, 200)
