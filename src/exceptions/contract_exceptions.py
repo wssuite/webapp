554132df3fb308b94fbbf667f4d17baadb9105a6
Mark Bekhet <markonsy72@gmail.com>
2023-02-24 12:10:00 -0500

    Edit message for contract contradiction error

--- src/exceptions/contract_exceptions.py
+++ src/exceptions/contract_exceptions.py
@@ -1,11 +1,11 @@
 from src.exceptions.project_base_exception import ProjectBaseException
-from error_msg import contract_creation_error
+from error_msg import contract_contradiction
 
 
 class ContractContradictionException(ProjectBaseException):
 
     def __init__(self, added_contract, existing_contract, constraints):
-        msg = contract_creation_error.format(
+        msg = contract_contradiction.format(
             added_contract, existing_contract,
             constraints)
         super().__init__(msg)
