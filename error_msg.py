dfc3fa5da6900cdf326317733cb213147b597081
Mark Bekhet <markonsy72@gmail.com>
2023-02-24 15:49:35 -0500

    Adjust the message in shiftTypeAlreadyExistException

--- error_msg.py
+++ error_msg.py
@@ -3,3 +3,4 @@ version_not_found = "Version not found"
 version_required = "Version required please make sure" \
                    " to specify version in the parameters"
 shift_already_exists = "The shift {} already exists"
+shift_type_already_exists = "The shift type {} already exists"
--- src/exceptions/shift_exceptions.py
+++ src/exceptions/shift_exceptions.py
@@ -1,5 +1,5 @@
 from src.exceptions.project_base_exception import ProjectBaseException
-from error_msg import shift_already_exists
+from error_msg import shift_already_exists, shift_type_already_exists
 
 
 class ShiftAlreadyExistException(ProjectBaseException):
@@ -12,5 +12,5 @@ class ShiftTypeAlreadyExistException(
     ProjectBaseException
 ):
     def __init__(self, shift_type_name):
-        msg = shift_already_exists.format(shift_type_name)
+        msg = shift_type_already_exists.format(shift_type_name)
         super().__init__(msg)
