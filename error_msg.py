732df1b99b9f5baae887fb9aaf006c59be1389da
Mark Bekhet <markonsy72@gmail.com>
2023-02-24 09:53:08 -0500

    Create the dao for the shift

--- error_msg.py
+++ error_msg.py
@@ -2,3 +2,4 @@ no_solution_found = "No Solution found"
 version_not_found = "Version not found"
 version_required = "Version required please make sure" \
                    " to specify version in the parameters"
+shift_already_exists = "The shift {} already exists"
---
+++ src/exceptions/shift_exceptions.py
@@ -0,0 +1,8 @@
+from src.exceptions.project_base_exception import ProjectBaseException
+from error_msg import shift_already_exists
+
+
+class ShiftAlreadyExistException(ProjectBaseException):
+    def __init__(self, shift_name):
+        msg = shift_already_exists.format(shift_name)
+        super().__init__(msg)
