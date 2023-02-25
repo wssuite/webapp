ed46886aec2afb05ef8747e88efd7bf0a4c47559
Mark Bekhet <markonsy72@gmail.com>
2023-02-25 11:17:18 -0500

    Add endpoints to nurse controller

--- src/controllers/nurse_controller.py
+++ src/controllers/nurse_controller.py
@@ -1,19 +1,47 @@
-from flask import Blueprint, request
-from src.dao.nurse_dao import NurseDao
+from flask import Blueprint, request, Response
+from src.dao.nurse_dao import NurseDao, Nurse
 from src.dao.abstract_dao import connect_to_db
+from constants import nurse_username
 
 mod = Blueprint("nurse_controller", __name__, url_prefix="/nurse")
-
 
 nurse_dao = NurseDao(connect_to_db())
 
 
 @mod.route("/add", methods=["POST"])
-def register_nurse():
+def add_nurse():
     nurse_dict = request.json
     return str(nurse_dao.insert_one(nurse_dict).inserted_id)
 
 
 @mod.route("/fetchAll", methods=["GET"])
-def fetch_all_nurses():
+def get_all_nurses():
     return nurse_dao.fetch_all()
+
+
+@mod.route("/fetchByUsername", methods=["GET"])
+def get_nurse_by_username():
+    username = request.args[nurse_username]
+    return nurse_dao.find_nurse_by_username(username)
+
+
+@mod.route("/remove", methods=["DELETE"])
+def delete_nurse():
+    username = request.args[nurse_username]
+    nurse_dao.remove_nurse(username)
+    return Response("OK", 200)
+
+
+@mod.route("/fetchAllUsernames", methods=["GET"])
+def get_nurses_usernames():
+    all_nurses = nurse_dao.fetch_all()
+    return [nurse[nurse_username]
+            for nurse in all_nurses]
+
+
+@mod.route("/update", methods=["PUT"])
+def update_nurse():
+    update_dict = request.json
+    nurse = Nurse().from_json(update_dict)
+    nurse_dao.update_nurse(nurse.db_json())
+    return Response("OK", 200)
