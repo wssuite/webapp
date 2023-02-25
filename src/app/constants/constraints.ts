34390eb870d1fd8c6def75a9797eaed3e0fe2c09
Niczyporuk-ma <malgorzata-anna.niczyporuk@polymtl.ca>
2023-02-25 13:32:05 -0500

    Created model and constants

---
+++ src/app/constants/constraints.ts
@@ -0,0 +1,5 @@
+export const BASE_VALUE = "0";
+export const IDENTICAL_WEEKEND_DISPLAY_ID = "identicalWeekend";
+export const IDENTICAL_WEEKEND_DISPLAY_NAME = "Identical Weekend";
+export const COMPLETE_WEEKEND_DISPLAY_ID = "completeWeekend";
+export const COMPLETE_WEEKEND_DISPLAY_NAME = "Complete Weekend";
