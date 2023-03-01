065bc8969bc1582d22a8b36d61c8c60dda819ccb
zoeparadis <zoe.paradis@polymtl.ca>
2023-02-28 19:02:43 -0500

    add log in button

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MAIN_MENU } from 'src/app/constants/app-routes';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string;
+  password: string;
+  usernameControlForm = new FormGroup({
+    username: new FormControl(null, Validators.required),
+  });
+  passwordControlForm = new FormGroup({
+    password: new FormControl(null, Validators.required),
+  });
 
+  constructor(private router: Router) {
+    this.username = "";
+    this.password = "";
+  }
+
+  login() {
+    console.log("login" + this.username + this.password);
+    this.router.navigate(["/" + MAIN_MENU]);
+  }
 }
