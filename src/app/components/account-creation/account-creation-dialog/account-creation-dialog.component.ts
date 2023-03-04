99a403eb77fff9b7bf5e744e1445837e1b135a34
zoeparadis <zoe.paradis@polymtl.ca>
2023-03-04 11:39:06 -0500

    add input creation new account

--- src/app/components/account-creation/account-creation-dialog/account-creation-dialog.component.ts
+++ src/app/components/account-creation/account-creation-dialog/account-creation-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Account } from 'src/app/models/Account';
 
@@ -9,7 +10,23 @@ import { Account } from 'src/app/models/Account';
 })
 export class AccountCreationDialogComponent {
 
+  username: string;
+  password: string;
+  passwordConfirmation: string;
+  usernameControlForm = new FormGroup({
+    username: new FormControl(null, Validators.required),
+  });
+  passwordControlForm = new FormGroup({
+    password: new FormControl(null, Validators.required),
+  });
+  passwordConfirmationControlForm = new FormGroup({
+    passwordConfirmation: new FormControl(null, Validators.required),
+  });
+
   constructor(public dialogRef: MatDialogRef<AccountCreationDialogComponent >, @Inject(MAT_DIALOG_DATA) public data: Account ) { 
+    this.username = "";
+    this.password = "";
+    this.passwordConfirmation = "";
   }
   
 
