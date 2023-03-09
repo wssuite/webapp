c9ca7301101a832c45f97f75bf64e0202d2f869a
Mark Bekhet <markonsy72@gmail.com>
2023-03-09 15:17:45 -0500

    Integrate the profile reference inside the shift type handler

--- src/handlers/shift_type_handler.py
+++ src/handlers/shift_type_handler.py
@@ -23,10 +23,10 @@ class ShiftTypeHandler:
         self.contract_dao = ContractDao(mongo)
         self.shift_dao = ShiftDao(mongo)
 
-    def verify_shifts_exist(self, shifts):
+    def verify_shifts_exist(self, shifts, profile):
         not_exist_shifts = []
         for shift in shifts:
-            exist = self.shift_dao.exist(shift)
+            exist = self.shift_dao.exist(shift, profile)
             if exist is False:
                 not_exist_shifts.append(shift)
         if len(not_exist_shifts) > 0:
@@ -35,38 +35,38 @@ class ShiftTypeHandler:
     def add(self, token, json):
         verify_token(token, self.user_dao)
         shift_type = ShiftType().from_json(json)
-        self.verify_shifts_exist(shift_type.shifts)
+        self.verify_shifts_exist(shift_type.shifts, shift_type.profile)
         self.shift_type_dao.insert_one_if_not_exist(shift_type.db_json())
-        add_shift_in_work_shift_group(shift_type.name, self.shift_group_dao)
+        add_shift_in_work_shift_group(shift_type.name, self.shift_group_dao, shift_type.profile)
 
     def update(self, token, json):
         verify_token(token, self.user_dao)
         shift_type = ShiftType().from_json(json)
-        self.verify_shifts_exist(shift_type.shifts)
+        self.verify_shifts_exist(shift_type.shifts, shift_type.profile)
         self.shift_type_dao.update(shift_type.db_json())
 
-    def delete(self, token, name):
+    def delete(self, token, name, profile):
         verify_token(token, self.user_dao)
         usage = []
-        usage.extend(self.contract_dao.get_including_shifts([name]))
-        usage.extend(self.shift_group_dao.get_including_shifts([name]))
+        usage.extend(self.contract_dao.get_including_shifts([name], profile))
+        usage.extend(self.shift_group_dao.get_including_shifts([name], profile))
         if len(usage) > 1:
             raise CannotDeleteShiftType(name)
-        self.shift_type_dao.remove(name)
-        remove_shift_from_work_shift_group(name, self.shift_group_dao)
+        self.shift_type_dao.remove(name, profile)
+        remove_shift_from_work_shift_group(name, self.shift_group_dao, profile)
 
-    def get_by_name(self, token, name):
+    def get_by_name(self, token, name, profile):
         verify_token(token, self.user_dao)
-        shift_type_dict = self.shift_type_dao.find_by_name(name)
+        shift_type_dict = self.shift_type_dao.find_by_name(name, profile)
         if shift_type_dict is None:
             raise ShiftNotExist(name)
         return ShiftType().from_json(shift_type_dict).to_json()
 
-    def get_all(self, token):
+    def get_all(self, token, profile):
         verify_token(token, self.user_dao)
-        return self.shift_type_dao.fetch_all()
+        return self.shift_type_dao.fetch_all(profile)
 
-    def get_all_names(self, token):
+    def get_all_names(self, token, profile):
         return [
-            shift_type[shift_type_name] for shift_type in self.get_all(token)
+            shift_type[shift_type_name] for shift_type in self.get_all(token, profile)
         ]
--- test/handlers/test_shift_type_handler.py
+++ test/handlers/test_shift_type_handler.py
@@ -6,7 +6,7 @@ from test_constants import (
     test_work_shift_group,
     late_shift,
     early_shift,
-    full_time_contract_with_day_shift_type,
+    full_time_contract_with_day_shift_type, profile1
 )
 from src.dao.abstract_dao import connect_to_fake_db
 from unittest import TestCase
@@ -39,57 +39,57 @@ class TestShiftHandler(TestCase):
         pass
 
     def test_add_new_shift_type_if_included_shift_dont_exist_raise_error(self):
-        actual_before = self.handler.get_all_names(random_hex)
+        actual_before = self.handler.get_all_names(random_hex, profile1)
         expected_before = ["Day"]
         self.assertEqual(expected_before, actual_before)
         with self.assertRaises(ShiftNotExist):
             self.handler.add(random_hex, night_shift_type)
-        actual_after = self.handler.get_all_names(random_hex)
+        actual_after = self.handler.get_all_names(random_hex, profile1)
         self.assertEqual(expected_before, actual_after)
 
     def test_add_new_shift_type_if_included_shift_exist_gets_added(self):
-        actual_before = self.handler.get_all_names(random_hex)
+        actual_before = self.handler.get_all_names(random_hex, profile1)
         expected_before = ["Day"]
         self.assertEqual(expected_before, actual_before)
         self.handler.shift_dao.insert_one_if_not_exist(late_shift)
         self.handler.add(random_hex, night_shift_type)
-        actual_after = self.handler.get_all_names(random_hex)
+        actual_after = self.handler.get_all_names(random_hex, profile1)
         expected_after = ["Day", "Night"]
         self.assertEqual(expected_after, actual_after)
 
     def test_update_shift_type_to_include_nonexistent_shift_raise_error(self):
-        shift_before = self.handler.get_by_name(random_hex, "Day")
+        shift_before = self.handler.get_by_name(random_hex, "Day", profile1)
         update = shift_before.copy()
         update[shift_type_shifts_lists] = ["Early", "MidDay"]
         with self.assertRaises(ShiftNotExist):
             self.handler.update(random_hex, update)
-        shift_after = self.handler.get_by_name(random_hex, "Day")
+        shift_after = self.handler.get_by_name(random_hex, "Day", profile1)
         self.assertEqual(shift_before, shift_after)
 
     def test_update_shift_type_to_not_have_any_shifts_succeed(self):
-        shift_before = self.handler.get_by_name(random_hex, "Day")
+        shift_before = self.handler.get_by_name(random_hex, "Day", profile1)
         update = shift_before.copy()
         update[shift_type_shifts_lists] = []
         self.handler.update(random_hex, update)
-        shift_after = self.handler.get_by_name(random_hex, "Day")
+        shift_after = self.handler.get_by_name(random_hex, "Day", profile1)
         self.assertNotEqual(shift_before, shift_after)
 
     def test_get_shift_type_if_not_exist_raise_error(self):
         with self.assertRaises(ShiftNotExist):
-            self.handler.get_by_name(random_hex, "MidDay")
+            self.handler.get_by_name(random_hex, "MidDay", profile1)
 
     def test_delete_shift_type_has_dependent_objects_raise_error(self):
-        all_shifts_before = self.handler.get_all(random_hex)
+        all_shifts_before = self.handler.get_all(random_hex, profile1)
         with self.assertRaises(CannotDeleteShiftType):
-            self.handler.delete(random_hex, "Day")
-        all_shifts_after = self.handler.get_all(random_hex)
+            self.handler.delete(random_hex, "Day", profile1)
+        all_shifts_after = self.handler.get_all(random_hex, profile1)
         self.assertEqual(all_shifts_before, all_shifts_after)
 
     def test_delete_shift_type_with_no_dependent_objects_succeed(self):
         self.handler.shift_dao.insert_one_if_not_exist(late_shift)
         self.handler.add(random_hex, night_shift_type.copy())
-        all_shift_types_before = self.handler.get_all(random_hex)
-        self.handler.delete(random_hex, "Night")
-        all_shift_types_after = self.handler.get_all(random_hex)
+        all_shift_types_before = self.handler.get_all(random_hex, profile1)
+        self.handler.delete(random_hex, "Night", profile1)
+        all_shift_types_after = self.handler.get_all(random_hex, profile1)
         self.assertEqual(2, len(all_shift_types_before))
         self.assertEqual(1, len(all_shift_types_after))
