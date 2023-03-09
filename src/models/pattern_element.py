1a31014884fcda9ff62eaa2aafdbb9e17ebe585f
Mark Bekhet <markonsy72@gmail.com>
2023-03-09 07:55:35 -0500

    Change the names for pattern lement attributes

--- src/models/pattern_element.py
+++ src/models/pattern_element.py
@@ -4,8 +4,8 @@ from constants import pattern_element_shift, pattern_element_day
 
 
 class PatternElement(Jsonify):
-    shift = ListField(str, serialized_name=pattern_element_shift)
-    day = ListField(str, serialized_name=pattern_element_day)
+    shifts = ListField(str, serialized_name=pattern_element_shift)
+    days = ListField(str, serialized_name=pattern_element_day)
 
     def get_shift(self):
-        return self.shift
+        return self.shifts
