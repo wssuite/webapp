2de6c829a1781e8c7a0923e85e0706b694fe171f
Mark Bekhet <markonsy72@gmail.com>
2023-03-09 10:54:38 -0500

    Integrate the profile reference in the nurse dao

--- test/dao/test_nurse_dao.py
+++ test/dao/test_nurse_dao.py
@@ -48,7 +48,7 @@ class TestNurseDao(TestCase):
         nurse_dict = self.nurse_dict.copy()
         nurse_dict[nurse_inherited_contracts] = []
         nurse_dict[nurse_id] = str(nurse_id_inserted.inserted_id)
-        all_nurses = self.dao.fetch_all()
+        all_nurses = self.dao.fetch_all(profile1)
         self.assertEqual(1, len(all_nurses))
         self.assertEqual(nurse_dict, all_nurses[0])
 
@@ -65,11 +65,11 @@ class TestNurseDao(TestCase):
         nurse_dict = self.nurse_dict.copy()
         nurse_dict[nurse_id] = str(inserted_id.inserted_id)
         nurse_dict[nurse_inherited_contracts] = []
-        parTime = self.dao.get_with_contracts(["ParTime"])
-        fullTime = self.dao.get_with_contracts(["FullTime"])
-        self.assertEqual(0, len(parTime))
-        self.assertEqual(1, len(fullTime))
-        self.assertEqual(nurse_dict, fullTime[0])
+        part_time = self.dao.get_with_contracts(["ParTime"], profile1)
+        full_time = self.dao.get_with_contracts(["FullTime"], profile1)
+        self.assertEqual(0, len(part_time))
+        self.assertEqual(1, len(full_time))
+        self.assertEqual(nurse_dict, full_time[0])
 
     def test_update_nurse(self):
         nurse = Nurse().from_json(self.nurse_dict)
@@ -78,15 +78,16 @@ class TestNurseDao(TestCase):
         result = self.nurse_update.copy()
         result[nurse_id] = str(inserted_id.inserted_id)
         nurse_updated = self.dao.find_by_username(
-            self.nurse_dict[nurse_username]
+            self.nurse_dict[nurse_username],
+            self.nurse_dict[profile]
         )
         self.assertEqual(result, nurse_updated)
 
     def test_remove_nurse(self):
         nurse = Nurse().from_json(self.nurse_dict)
         self.dao.insert_one(nurse.db_json())
-        all_nurses_before = self.dao.fetch_all()
-        self.dao.remove(nurse.username)
-        all_nurses_after = self.dao.fetch_all()
+        all_nurses_before = self.dao.fetch_all(profile1)
+        self.dao.remove(nurse.username, nurse.profile)
+        all_nurses_after = self.dao.fetch_all(profile1)
         self.assertEqual(1, len(all_nurses_before))
         self.assertEqual(0, len(all_nurses_after))
