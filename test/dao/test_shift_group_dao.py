c70afcbc12c572c6fa024e5acb6e245a124d1e25
Mark Bekhet <markonsy72@gmail.com>
2023-03-13 08:44:58 -0400

    Adjust the shift group handler to account for shift types

--- test/dao/test_shift_group_dao.py
+++ test/dao/test_shift_group_dao.py
@@ -65,6 +65,14 @@ class TestShiftGroupDao(TestCase):
         self.assertEqual(1, len(early_shift_group))
         self.assertEqual(self.shift_group, early_shift_group[0])
 
+    def test_get_shift_groups_including_shift_types(self):
+        self.dao.insert_one_if_not_exist(self.shift_group.copy())
+        night_shift_group = self.dao.get_including_shift_types(["Night"], profile1)
+        day_shift_group = self.dao.get_including_shift_types(["Day"], profile1)
+        self.assertEqual(0, len(night_shift_group))
+        self.assertEqual(1, len(day_shift_group))
+        self.assertEqual(self.shift_group, day_shift_group[0])
+
     def test_add_shift_to_nonexistent_shift_group_raise_error(self):
         with self.assertRaises(ShiftNotExist):
             self.dao.add_shift_to_shift_group_list("Work", "Late", profile1)
@@ -103,6 +111,19 @@ class TestShiftGroupDao(TestCase):
         )
         self.assertEqual(
             ["Midnight"], shift_group_after[shift_group_shifts_list]
+        )
+
+    def test_remove_shift_type_from_existing_group_succeed(self):
+        self.dao.insert_one_if_not_exist(self.shift_group.copy())
+        shift_group_before = self.dao.find_by_name("Work", profile1)
+        self.dao.delete_shift_type_from_shift_group_list("Work", "Day", profile1)
+        shift_group_after = self.dao.find_by_name("Work", profile1)
+        self.assertEqual(
+            ["Day"],
+            shift_group_before[shift_group_shift_types],
+        )
+        self.assertEqual(
+            [], shift_group_after[shift_group_shift_types]
         )
 
     def test_delete_all_shift_groups_from_profile_deletes_items_for_profile(
