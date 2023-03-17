697736fe779b996e7259ff5c4ff51856e3cd438b
zoeparadis <zoe.paradis@polymtl.ca>
2023-03-17 19:00:20 -0400

    change weight associate with button change

--- src/app/components/nurse-preference/nurse-preference.component.ts
+++ src/app/components/nurse-preference/nurse-preference.component.ts
@@ -11,7 +11,8 @@
     shifts: string[];
     timetable: string[];
     buttonStates: {[key: string]: {[key: string]: string}}; // state of each button
-  
+    selectedShifts: {shift: string, date: string, weight: number}[] = []; // selected shifts and dates
+    
     constructor(){
       this.buttonStates = {};
       this.shifts = ["early", "late", "evening", "night"];
@@ -29,14 +30,25 @@
       switch(currentState){
         case "check_box_outline_blank": {
           this.buttonStates[shift][date] = "check_box";
+          const weight = 1;
+          this.selectedShifts.push({shift, date, weight}); // add to selected shifts
           break;
         }
         case "check_box": {
+          const weight = 0.5;
+          const index = this.selectedShifts.findIndex(selectedShift => 
+            selectedShift.shift === shift && selectedShift.date === date);
+          this.selectedShifts[index].weight = weight;
           this.buttonStates[shift][date] = "indeterminate_check_box";
           break;
         }
         case "indeterminate_check_box": {
           this.buttonStates[shift][date] = "check_box_outline_blank";
+          // remove from selected shifts
+          const weight = 0.5;
+          const index = this.selectedShifts.findIndex(selectedShift => 
+            selectedShift.shift === shift && selectedShift.date === date);
+          this.selectedShifts.splice(index, 1);
           break;
         }
         default: {
@@ -44,6 +56,7 @@
           break;
         }
       }
+      console.log("shift selected", this.selectedShifts);
     }
   
     getButtonState(shift: string, date: string): string {
