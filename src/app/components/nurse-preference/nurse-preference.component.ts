347f995ce320e0897bcf206edf90fccdddd80d40
zoeparadis <zoe.paradis@polymtl.ca>
2023-03-18 16:25:20 -0400

    change weight value without unchecking if they are different and uncheck checkbox when it is the same weight

--- src/app/components/nurse-preference/nurse-preference.component.ts
+++ src/app/components/nurse-preference/nurse-preference.component.ts
@@ -9,30 +9,49 @@ import { FormControl, Validators } from '@angular/forms';
     styleUrls: ['./nurse-preference.component.css']
   })
   export class NursePreferenceComponent {
-    weight: string;
-    possibleShifts: string[];
-    timetable: string[];
-    selectedShifts: {shift: string, date: string, weight: string}[] = [];
-    possibleNurses: string [];
-    selectedNurse: string[] = [];
-    schedulePreference: {[key: string]: {[key: string]: {pref:boolean, weight: string}}} = {};
-    nurseSelectorFormControl = new FormControl();
-  
-    constructor(){
-      this.weight = '';
-      this.possibleShifts = ["early", "late", "evening", "night"];
-      this.timetable = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-      this.possibleNurses = ["Nurse A", "Nurse B", "Nurse C", "Nurse D"];
-    }
+  weight: string;
+  possibleShifts: string[];
+  timetable: string[];
+  selectedShifts: { shift: string, date: string, weight: string }[] = [];
+  possibleNurses: string[];
+  selectedNurse: string[] = [];
+  preferences: { [shift: string]: { [date: string]: { pref: boolean, weight: string } } } = {};
+  nurseSelectorFormControl = new FormControl();
 
-    updatePreferences( shift: string, date: string, checked: boolean) {
-        if (!this.schedulePreference[shift]) {
-          this.schedulePreference[shift] = {};
-        }
-        const weight = checked ? this.weight : '';
-        this.schedulePreference[shift][date] = { pref: checked, weight };
-      console.log("nurse preference", this.schedulePreference);
-      console.log("nurse selected", this.selectedNurse);
+  constructor() {
+    this.weight = '';
+    this.possibleShifts = ["early", "late", "evening", "night"];
+    this.timetable = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+    this.possibleNurses = ["Nurse A", "Nurse B", "Nurse C", "Nurse D"];
+    for (const shift of this.possibleShifts) {
+      this.preferences[shift] = {};
+      for (const date of this.timetable) {
+        this.preferences[shift][date] = { pref: false, weight: '' };
+      }
     }
   }
-  
+
+  updatePreferences(shift: string, date: string) {
+    if (!this.preferences[shift]) {
+      this.preferences[shift] = {};
+    }
+    if (!this.preferences[shift][date]) {
+      this.preferences[shift][date] = { pref: false, weight: '' };
+    }
+    let pref = this.preferences[shift][date].pref;
+    const weight =  this.weight;
+    if(this.weight ===  this.preferences[shift][date].weight){
+      pref = false;  
+      console.log("value pref", pref)
+    } else {
+      pref = true;
+      console.log("ici", this.weight ===  this.preferences[shift][date].weight, this.weight, this.preferences[shift][date].weight)
+    }
+    this.preferences[shift][date] = { pref, weight };
+    console.log("nurse preference", this.preferences);
+    console.log("nurse selected", this.selectedNurse);
+  }
+
+
+}
+
