4d790a3929cf10978ad766196b66a73588ee28cf
Mark Bekhet <markonsy72@gmail.com>
2023-03-23 18:21:28 -0400

    Create the endpoints to get the solution

--- src/controllers/schedule_controller.py
+++ src/controllers/schedule_controller.py
@@ -6,7 +6,7 @@ from src.utils.file_system_manager import FileSystemManager
 from src.models.schedule import Schedule
 from flask import request
 from src.exceptions.project_base_exception import ProjectBaseException
-from constants import user_token, profile, start_date, end_date
+from constants import user_token, profile, start_date, end_date, version
 from src.dao.abstract_dao import DBConnection
 from src.handlers.schedule_handler import ScheduleHandler
 
@@ -62,9 +62,9 @@ def export_problem():
         profile_name = request.args[profile]
         start = request.args[start_date]
         end = request.args[end_date]
-        version = request.args["version"]
+        v = request.args[version]
         path = schedule_handler.get_input_problem_path(
-            token, profile_name, start, end, version
+            token, profile_name, start, end, v
         )
         problem_str = ""
         with open(path) as file:
@@ -73,5 +73,51 @@ def export_problem():
                 problem_str += line
 
         return {'content': problem_str}
+    except ProjectBaseException as e:
+        return Response(e.args, 500)
+
+
+@mod.route("/regenerate", methods=["POST"])
+def regenerate_schedule():
+    try:
+        token = request.args[user_token]
+        json = request.json
+        v = request.args[version]
+        return schedule_handler.regenerate_schedule(token, json, v)
+    except ProjectBaseException as e:
+        return Response(e.args, 500)
+
+
+@mod.route("/getDetailedSolution", methods=["GET"])
+def get_detailed_solution():
+    try:
+        token = request.args[user_token]
+        start = request.args[start_date]
+        end = request.args[end_date]
+        profile_name = request.args[profile]
+        v = request.args[version]
+        return schedule_handler.get_detailed_solution(
+            token, start, end, profile_name, v)
+    except ProjectBaseException as e:
+        return Response(e.args, 500)
+
+
+@mod.route("/getAllSolutions", methods=["GET"])
+def get_all_solutions():
+    try:
+        token = request.args[user_token]
+        profile_name = request.args[profile]
+        return schedule_handler.get_all_solutions(token, profile_name)
+    except ProjectBaseException as e:
+        return Response(e.args, 500)
+
+
+@mod.route("/getLatestSolutions", methods=["GET"])
+def get_latest_solutions():
+    try:
+        token = request.args[user_token]
+        profile_name = request.args[profile]
+        return schedule_handler.get_latest_solutions_versions(
+            token, profile_name)
     except ProjectBaseException as e:
         return Response(e.args, 500)
