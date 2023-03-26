1ed290b8851a85b1c1bf499e90534ca0e8285959
Niczyporuk-ma <malgorzata-anna.niczyporuk@polymtl.ca>
2023-03-25 21:55:26 -0400

    Add constraint descriptions

--- src/app/constants/constraintsDescriptions.ts
+++ src/app/constants/constraintsDescriptions.ts
@@ -1,14 +1,14 @@
-export const UNWANTED_PATTERNS_DESCRIPTION = "temporary placeholder";
-export const TOTAL_WEEKENDS_IN_FOUR_WEEKS_DESCRIPTION = "temporary placeholder";
-export const ALTERNATIVE_SHIFT_DESCRIPTION="temporary placeholder";
-export const FREE_DAYS_AFTER_SHIFT_DESCRIPTION = "temporary placeholder";
-export const MIN_MAX_CONSECUTIVE_SHIFT_TYPE_DESCRIPTION = "temporary placeholder";
-export const IDENTICAL_WEEKEND_DESCRIPTION = "temporary placeholder";
-export const COMPLETE_WEEKEND_DESCRIPTION = "temporary placeholder";
-export const MIN_MAX_NUM_ASSIGNMENTS_IN_FOUR_WEEKS_DESCRIPTION = "temporary placeholder";
-export const MIN_MAX_CONSECUTIVE_WORKING_WEEKENDS_DESCRIPTION = "temporary placeholder";
-export const UNWANTED_SKILLS_DESCRIPTION="temporary placeholder";
-export const MIN_MAX_WORKING_HOURS_IN_FOUR_WEEKS_DESCRIPTION = "temporary placeholder"
+export const UNWANTED_PATTERNS_DESCRIPTION = "Declare various shift successions you want to avoid within the planning horizon.";
+export const TOTAL_WEEKENDS_IN_FOUR_WEEKS_DESCRIPTION = "Declare the minimum and maximum number of weekends that can be assigned to a nurse within four weeks.";
+export const ALTERNATIVE_SHIFT_DESCRIPTION="Declare all shifts that you might want to see less frequently withn the planing horizon.";
+export const FREE_DAYS_AFTER_SHIFT_DESCRIPTION = "Declare the number of free days a nurse would get after working a specific shift.";
+export const MIN_MAX_CONSECUTIVE_SHIFT_TYPE_DESCRIPTION = "Declare the minimum and maximum number of consecutive shifts a nurse can work within the planning horizon.";
+export const IDENTICAL_WEEKEND_DESCRIPTION = "Impose identical shifts for all weekends within your working horizon.";
+export const COMPLETE_WEEKEND_DESCRIPTION = "Impose that a nurse has to work the whole weekend.";
+export const MIN_MAX_NUM_ASSIGNMENTS_IN_FOUR_WEEKS_DESCRIPTION = "Declare the minimum and maximum number of assignments a nurse can work within four weeks.";
+export const MIN_MAX_CONSECUTIVE_WORKING_WEEKENDS_DESCRIPTION = "Declare the minimum and maximum number of consecutive weekends a nurse can work within the planning horizon.";
+export const UNWANTED_SKILLS_DESCRIPTION="Declare all skills that you might want to see less frequently withn the planing horizon.";
+export const MIN_MAX_WORKING_HOURS_IN_FOUR_WEEKS_DESCRIPTION = "Declare the minimum and maximum hours that a nurse can work within four weeks."
 
 
 
