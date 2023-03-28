3d92c7fe7974a8bcf3a6ddde9d4c6e60b02133b1
Mark Bekhet <markonsy72@gmail.com>
2023-03-28 10:56:36 -0400

    Send the progress status of a process to the main server

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
-FROM legraina/bcp
+FROM legraina/nurse-scheduler
 USER root
 RUN apt-get --allow-releaseinfo-change update
 RUN apt-get update
 RUN apt-get install python3-venv -y
-RUN python3 -m venv venv
+RUN python3 -m venv /venv
 RUN /venv/bin/pip install Flask
-RUN mkdir invoker
-COPY . invoker/.
+RUN /venv/bin/pip install requests
+RUN mkdir /invoker
+COPY . /invoker/.
 EXPOSE 5000
--- src/handler.py
+++ src/handler.py
@@ -1,3 +1,5 @@
+import requests
+
 from .file_writer import FileManager
 import os
 import subprocess
@@ -11,15 +13,32 @@ waiting_fm = FileManager(os.path.join(running_dir, 'waiting.json'))
 running_dict = {}
 
 
+def extract_version_info_from_path(path):
+    separator = os.path.sep
+    path_list = path.split(separator)
+    json = {"version": path_list[-1]}
+    start_end = path_list[-2].split("_")
+    json["startDate"] = start_end[0]
+    json["endDate"] = start_end[1]
+    json["profile"] = path_list[-3]
+    return json
+
+
 def run_scheduler(path, counter):
     """
         TODO: launch a request to the ffs to update the status
             to In progress
         """
+    json = extract_version_info_from_path(path)
+    requests.post(
+        f"http://{sys.argv[3]}/schedule/updateStatus",
+        json=json
+    )
+    json["state"] = "In Progress"
     running_fm.add_item(path, counter)
     input_file_path = os.path.join(path, "input.json")
-    subprocess.Popen(["sleep", "1m"])
-    proc = subprocess.Popen(["cat", f"{input_file_path}"])
+    proc = subprocess.Popen(["sleep", "2m"])
+    subprocess.Popen(["cat", f"{input_file_path}"])
     proc.wait()
     print(proc.returncode)
     callback(path, proc.returncode)
@@ -30,18 +49,28 @@ def callback(path, status):
     TODO: the callback will launch a request to the ffs to inform
         it with the status of the schedule
     """
+    json = extract_version_info_from_path(path)
     if status == 0:
         """return success status"""
+        json["state"] = "Success"
     else:
         """return Failed status"""
+        json["state"] = "Success"
+
+    requests.post(
+        f"http://{sys.argv[3]}/schedule/updateStatus",
+        json=json
+    )
     running_fm.pop_item_by_key(path)
+    print(running_dict)
+    running_dict.pop(path)
 
 
 def schedule(full_path, counter):
     process = multiprocessing.Process(target=run_scheduler,
                                       args=(full_path, counter, ))
+    running_dict[full_path] = process
     process.start()
-    return process
 
 
 def add_to_waiting(key, value):
@@ -56,5 +85,4 @@ def schedule_waiting():
     if len(running_dict) < int(sys.argv[2]):
         item = pop_from_waiting()
         if item is not None:
-            process = schedule(item[0], item[1])
-            running_dict[item[0]] = process
+            schedule(item[0], item[1])
