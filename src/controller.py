fe88727ed49a41131c219725c75a2e0a60b0e373
Mark Bekhet <markonsy72@gmail.com>
2023-03-30 19:25:19 -0400

    Add the url to receive the request to stop a schedule generation

--- src/controller.py
+++ src/controller.py
@@ -19,8 +19,15 @@ def test():
 def schedule():
     path = request.args["path"]
     path = path[1:]
-    print(path)
     full_path = os.path.join(base_directory, path)
-    print(full_path)
     add_to_waiting(full_path, 1)
     return Response(sys.argv[1], status=200)
+
+
+@mod.route("/stop", methods=["POST"])
+def stop():
+    path = request.args["path"]
+    path = path[1:]
+    full_path = os.path.join(base_directory, path)
+    """Delete the element from the running map"""
+    return Response("ok_message", status=200)
