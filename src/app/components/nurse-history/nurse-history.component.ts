5393f3dc397cfa8b709270e6b97c3eb0f90d5aeb
CarolineDesRochers <caroline.des-rochers@polymtl.ca>
2023-04-04 17:04:51 -0400

    add nurse-history component

---
+++ src/app/components/nurse-history/nurse-history.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nurse-history',
+  templateUrl: './nurse-history.component.html',
+  styleUrls: ['./nurse-history.component.css']
+})
+export class NurseHistoryComponent {
+
+}
