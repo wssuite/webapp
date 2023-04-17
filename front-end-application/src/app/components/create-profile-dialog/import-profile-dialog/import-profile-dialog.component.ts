5b1cfa4e4cb758d2fb874ee19ace6a192f6bbc4e
zoeparadis <zoe.paradis@polymtl.ca>
2023-04-17 10:12:29 -0400

    change css error dialog

--- front-end-application/src/app/components/create-profile-dialog/import-profile-dialog/import-profile-dialog.component.ts
+++ front-end-application/src/app/components/create-profile-dialog/import-profile-dialog/import-profile-dialog.component.ts
@@ -127,6 +127,9 @@ export class ImportProfileComponent implements OnInit{
   
   openErrorDialog(message: string){
     this.dialog.open(ErrorMessageDialogComponent, {
+      height: '45%',
+      width: '45%', 
+      position: {top:'20vh',left: '30%', right: '25%'},
       data:{message: message}
     })
   }
--- front-end-application/src/app/components/header/header.component.ts
+++ front-end-application/src/app/components/header/header.component.ts
@@ -154,6 +154,9 @@ export class HeaderComponent implements OnInit, AfterViewInit{
 
   openErrorDialog(message: string){
     this.dialog.open(ErrorMessageDialogComponent, {
+      height: '45%',
+      width: '45%', 
+      position: {top:'20vh',left: '30%', right: '25%'},
       data:{message: message},
     })    
   }
